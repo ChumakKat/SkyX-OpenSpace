07f0eecf9b3d32e758330a91de2d61a88a6fa7b5
Chumak Ekaterina <99873721+ChumakKat@users.noreply.github.com>
2023-04-19 19:36:08 +0600

    Add files via upload

---
+++ php/mailreserve.php
@@ -0,0 +1,65 @@
+<?php
+
+header('Content-type: text/html');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+//use PHPMailer\PHPMailer\SMTP;
+
+require "PHPMailer/src/PHPMailer.php";
+require "PHPMailer/src/Exception.php";
+//require "PHPMailer/src/SMTP.php";
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'utf-8';
+$mail->setLanguage('ru', 'PHPMailer/language/');
+$mail->isHTML(true);  
+
+//$mail-> isSMTP();
+
+$mail->setFrom('muzkat@mail.com', 'Заявка с сайта'); // от кого будет уходить письмо?
+$mail->addAddress('gf.muzkat@gmail.com');     // Кому будет уходить письмо 
+$mail->Subject = 'Заявка на бронирвание проведения мероприятия в SkyX!';
+
+$name = $_POST['name'];
+$soname = $_POST['soname'];
+$email = $_POST['email'];
+$organ = $_POST['organ'];
+$time = $_POST['time'];
+$date = $_POST['date'];
+
+
+
+// Тело письам
+$body = '<h1>Заявка на бронирование</h1>';
+
+if(trim(!empty($name))){
+    $body.='<p><strong>Имя:</strong> '.$name.'</p>';
+}
+if(trim(!empty($soname))){
+    $body.='<p><strong>Фамилия:</strong> '.$soname.'</p>';
+}
+if(trim(!empty($email))){
+    $body.='<p><strong>Email:</strong> '.$email.'</p>';
+}
+if(trim(!empty($organ))){
+    $body.='<p><strong>Название организации:</strong> '.$organ.'</p>';
+}
+if(trim(!empty($time))){
+    $body.='<p><strong>Время бронирования:</strong> '.$time.'</p>';
+}
+if(trim(!empty($date))){
+    $body.='<p><strong>Дата бронирования:</strong> '.$date.'</p>';
+}
+
+
+
+$mail->Body = $body; //Отправка
+
+if (!$mail->send()) {
+    echo 'Ошибка: '.$mail->ErrorInfo;
+} else {
+    echo '!OK!';
+}
+
+?>
